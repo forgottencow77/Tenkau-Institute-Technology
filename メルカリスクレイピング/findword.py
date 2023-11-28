285c10ce0f69312fb6bedb5a0bcf9ff6fa9148da
TENKAU <forgottencow77@gmail.com>
2023-11-28 11:08:10 +0900

    change

---
+++ メルカリスクレイピング/findword.py
@@ -0,0 +1,2 @@
+url = "https://jp.mercari.com/shops/product/sVaGG3ZoBPLP6vAr6MaonX"
+print("shops" in url)
--- メルカリスクレイピング/mercaritest.py
+++ メルカリスクレイピング/mercaritest.py
@@ -19,11 +19,11 @@ browser = webdriver.Chrome(options=options)
 browser.implicitly_wait(3)
 
 # キーワード設定
-KEYWORD = 'macbook pro 2019 i7 16gb 256gb 15inch'
-MAX_ITEMS = 1
+KEYWORD = input()
+MAX_ITEMS = 5
 
 def get_url():
-    # 売り切れ表示
+    # 売り切れのみ表示
     url = 'https://jp.mercari.com/search?keyword=' + KEYWORD + '&status=sold_out%7Ctrading'
     browser.get(url)
     browser.implicitly_wait(5)
@@ -42,27 +42,56 @@ def get_data():
     for item_url in item_url_ls:
         browser.get(item_url)
         time.sleep(3)
-        # 商品名
-        item_name = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > div.mer-spacing-b-12 > div > div > h1').text
-        #商品説明
-        item_ex = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(2) > div.merShowMore.mer-spacing-b-16 > div > pre').text
-        #画像取得
-        src = browser.find_element(By.CSS_SELECTOR, '#main > article > div.sc-a6b7d8a7-2.WrJQp > section > div > div > div > div > div.sc-a6b7d8a7-2.ishIRQ > div > div.slick-slider.slick-initialized > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div > div > figure > div.imageContainer__f8ddf3a2 > picture > img').get_attribute('src')
-        #値段取得
-        item_price = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > section:nth-child(2) > div > div > span:nth-child(2)').text
-        # 出品者の名前
-        owner_name = browser.find_element(By.CSS_SELECTOR, 'p[class="merText body__5616e150 primary__5616e150 bold__5616e150"]').text
 
-        data = {
-            '商品名': item_name,
-            '商品説明': item_ex,
-            '価格': item_price,
-            'URL': item_url,
-            '画像URL': src,
-            '出品者': owner_name,
-        }
+        # shopsのページかどうかを判定する
+        shops = ("shops" in item_url)
 
-        item_ls.append(data)
+        if shops:  # コロンを追加
+            # shopsのページの場合は、それより下のコードを実行する
+
+            # 商品名
+            item_name = browser.find_element(By.CSS_SELECTOR, '.heading__a7d91561.page__a7d91561').text
+            #商品説明
+            item_ex = browser.find_element(By.CSS_SELECTOR, "pre[class='merText body__5616e150 primary__5616e150']").text
+            #画像取得
+            src = browser.find_element(By.CSS_SELECTOR, "#main > article > div.sc-a6b7d8a7-2.WrJQp > section > div > div > div > div > div.sc-a6b7d8a7-2.ishIRQ > div.sc-e76b9217-0.vYGhZ > div.slick-slider.slick-initialized > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div > div > figure > div.imageContainer__f8ddf3a2 > picture > img").get_attribute('src')
+            #値段取得
+            item_price = browser.find_element(By.CSS_SELECTOR, "#product-info > section:nth-child(1) > section:nth-child(2) > div > div > span:nth-child(2)").text
+            # 出品者の名前
+            owner_name = browser.find_element(By.CSS_SELECTOR, "p[class='merText body__5616e150 primary__5616e150 bold__5616e150']").text
+
+            data = {
+                '商品名': item_name,
+                '商品説明': item_ex,
+                '価格': item_price,
+                'URL': item_url,
+                '画像URL': src,
+                '出品者': owner_name,
+            }
+            item_ls.append(data)
+            
+        else:
+            # 商品名
+            item_name = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > div.mer-spacing-b-12 > div > div > h1').text
+            #商品説明
+            item_ex = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(2) > div.merShowMore.mer-spacing-b-16 > div > pre').text
+            #画像取得
+            src = browser.find_element(By.CSS_SELECTOR, '#main > article > div.sc-a6b7d8a7-2.WrJQp > section > div > div > div > div > div.sc-a6b7d8a7-2.ishIRQ > div > div.slick-slider.slick-initialized > div.slick-list > div > div.slick-slide.slick-active.slick-current > div > div > div > div > figure > div.imageContainer__f8ddf3a2 > picture > img').get_attribute('src')
+            #値段取得
+            item_price = browser.find_element(By.CSS_SELECTOR, '#item-info > section:nth-child(1) > section:nth-child(2) > div > div > span:nth-child(2)').text
+            # 出品者の名前
+            owner_name = browser.find_element(By.CSS_SELECTOR, 'p[class="merText body__5616e150 primary__5616e150 bold__5616e150"]').text
+
+            data = {
+                '商品名': item_name,
+                '商品説明': item_ex,
+                '価格': item_price,
+                'URL': item_url,
+                '画像URL': src,
+                '出品者': owner_name,
+            }
+            item_ls.append(data)
+
 
 def main():
     get_url()
